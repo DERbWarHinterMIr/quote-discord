fa9e467c7e1fac1d958f96007142d26e24d09dfe
DERbWarHinterMIr <DERbWarHinterMIr@users.noreply.github.com>
2026-06-11 17:48:52 +0200

    new file:   assets/Sangetzu.jpg 	modified:   site.config.js

--- site.config.js
+++ site.config.js
@@ -1,5 +1,8 @@
 window.quoteSite = {
-  quote: 'The quieter you become, the more you are able to hear.',
+  quote: "What’s the difference between a White man and his Nigger ? And I’m not talking about obvious stuff, like one’s an animal and one’s a person and one has two legs and one has three . Form,Ability and Power. That’s what’s puzzling. If their Form,Ability and power are exactly the same, why should that one becomes the Master and leads them into battle while the other becomes the Slave and carries the Master ? So what’s the Characteristic which distinguishes these two beings? There’s only one answer… INSTINCT!\n ( and white rights )",
   author: 'Rumi',
   backgroundImage: 'assets/background.png',
 };
+
+
+
