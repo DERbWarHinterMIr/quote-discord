3f1b02782bd2ca75cececf17287149e388c2267f
DERbWarHinterMIr <DERbWarHinterMIr@users.noreply.github.com>
2026-06-11 18:30:18 +0200

    new file:   assets/hollow_Ichigo.jpg 	new file:   assets/try2.jpg 	modified:   demo-server.err.log 	modified:   site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 window.quoteSite = {
   quote: "What’s the difference between a White man and his Nigger ? \nAnd I’m not talking about obvious stuff, like one’s an animal and one’s a person and one has two legs and one has three . \nForm,Ability and Power. \nThat’s what’s puzzling. \nIf their Form,Ability and power are exactly the same, why should that one becomes the Master and leads them into battle while the other becomes the Slave and carries the Master ? So what’s the Characteristic which distinguishes these two beings? There’s only one answer… INSTINCT!\n ( and white rights )",
   author: 'Zangetsu',
-  backgroundImage: 'assets/Sangetzu.jpg',
+  backgroundImage: 'assets/try2.jpg',
 };
 
 
